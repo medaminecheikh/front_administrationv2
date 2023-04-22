1e38f116d159287fdf10ed49e4828b08211b177a
medaminecheikh <amine_cheikhh@outlook.fr>
2023-04-22 09:12:07 +0100

    add model

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {AuthService} from "../../services/auth/auth.service";
 import {MenuItem} from "primeng/api";
-import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-navbar',
@@ -14,58 +13,13 @@ export class NavbarComponent implements OnInit{
   username!:any;
   constructor(private router: Router,private authService:AuthService) {}
 
-  navigateToQuit() {
-    this.router.navigate(['/login']);
+  Logout() {
+    this.authService.logout();
   }
   ngOnInit(): void {
     this.username= this.authService.getCurrentUser()?.username;
 
-    this.items = [
 
-      {
-        label: 'Users',
-        icon: 'pi pi-fw pi-user',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-fw pi-user-plus'
-          },
-          {
-            label: 'Delete',
-            icon: 'pi pi-fw pi-user-minus'
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-fw pi-users',
-            items: [
-              {
-                label: 'Filter',
-                icon: 'pi pi-fw pi-filter',
-                items: [
-                  {
-                    label: 'Print',
-                    icon: 'pi pi-fw pi-print'
-                  }
-                ]
-              },
-              {
-                icon: 'pi pi-fw pi-bars',
-                label: 'List'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        separator: true
-      },
-      {
-        label: 'Logout',
-        icon: 'pi pi-fw pi-power-off',
-        command: () => this.navigateToQuit()
-      }
-
-    ];
   }
 
 
